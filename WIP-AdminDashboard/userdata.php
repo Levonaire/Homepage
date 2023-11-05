334dd81eeb0a59b2e651069c9d41ef4969fb8e71
Levonaire <149132999+Levonaire@users.noreply.github.com>
2023-11-05 23:21:56 +0800

    Add files via upload

---
+++ WIP-AdminDashboard/userdata.php
@@ -0,0 +1,21 @@
+<?php
+{?>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+    <table class="navbar">
+        <tr>
+            <td><a href="index.html"><button>Homepage</button></a></td>
+            <td><a href="userdata.php"><button>User Information</button></a></td>
+            <td><a href="settings.php"><button>Site Settings</button></a></td>
+        </tr>
+    </table>
+    <br>
+    <br>
+    <br>
+    <h1 class="h1users"> User Information Interface </h1>
+</body>
+</html>
+<?php }?>
